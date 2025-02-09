a810a32feadd6e842cb602b28a1a343cca9ef0ef
vermavikku <vermavikku1998@gmail.com>
2025-02-09 15:02:37 +0530

    updated entity

--- src/entities/Product.ts
+++ src/entities/Product.ts
@@ -3,17 +3,25 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 @Entity()
 export class Product {
   @PrimaryGeneratedColumn()
-  id: number;
+  id!: number; // Using "!" to tell TypeScript that this will be initialized by TypeORM
 
   @Column()
-  sku: string;
+  sku!: string;
 
   @Column()
-  name: string;
+  name!: string;
 
   @Column("decimal")
-  price: number;
+  price!: number;
 
   @Column("text", { array: true })
-  images: string[];
+  images!: string[];
+
+  // Optional: Add a constructor if you prefer explicit initialization
+  constructor(sku: string, name: string, price: number, images: string[]) {
+    this.sku = sku;
+    this.name = name;
+    this.price = price;
+    this.images = images;
+  }
 }
